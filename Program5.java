27a449fcf7069048e49d700fec334ebad1edeb22
aadityamittal <66875383+aadityamittal@users.noreply.github.com>
2020-06-13 21:32:56 +0530

    Add files via upload

---
+++ Program5.java
@@ -0,0 +1,44 @@
+package euler;
+
+public class Program5 {
+	public static void main(String[] arg) { 
+      int[] a = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}; 
+      System.out.println(lcm(a)); 	} 
+
+	public static long lcm(int[] array){ 
+	        long lcm = 1; 
+	        int d = 2; 
+	          
+	        while (true) { 
+	            int c = 0; 
+	            boolean loop = false; 
+	              
+	            for (int i = 0; i < array.length; i++) { 
+	  
+	                if (array[i] == 0) { 
+	                    return 0; 
+	                } 
+	               
+	                if (array[i] == 1) { 
+	                    c++; 
+	                }  
+	                if (array[i] % d == 0) { 
+	                    loop = true; 
+	                    array[i] = array[i] / d; 
+	                } 
+	            } 
+	            if (loop) { 
+	                lcm = lcm * d; 
+	            } 
+	            else { 
+	                d++; 
+	            }  
+	            if (c == array.length) { 
+	                return lcm; 
+	            } 
+	        } 
+	    } 
+	}
+//Output:232792560
+
+
